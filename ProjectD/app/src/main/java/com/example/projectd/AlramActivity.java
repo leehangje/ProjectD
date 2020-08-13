0b3792d2da9d8c5d078fac8c2143d109e8576476
이항제 <lhj2644@naver.com>
2020-08-13 15:40:21 +0900

    빈깡통 Activity & Xml

---
+++ ProjectD/app/src/main/java/com/example/projectd/AlramActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alram);
+    }
+}
---
+++ ProjectD/app/src/main/java/com/example/projectd/MdInsertActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MdInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_md_insert);
+    }
+}
