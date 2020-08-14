cf393b9002b32b3289546b43312765e8b1283b34
박준근 <dteam>
2020-08-14 17:23:39 +0900

    알림 수정후 올립니다.

--- ProjectD/app/src/main/java/com/example/projectd/AlramActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/AlramActivity.java
@@ -1,5 +1,6 @@
 package com.example.projectd;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class AlramActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alram);
+
+        ActionBar actionBar = getSupportActionBar();  //제목줄 객체 얻어오기
+        actionBar.setTitle("알림");  //액션바 제목설정
+        actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
+
     }
 }
--- ProjectD/app/src/main/java/com/example/projectd/QnaActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/QnaActivity.java
@@ -11,6 +11,7 @@ public class QnaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qna);
+
         ActionBar actionBar = getSupportActionBar();  //제목줄 객체 얻어오기
         actionBar.setTitle("QnA");  //액션바 제목설정
         actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
