349940f73ff5331aac3a040b13d12f67829fe61e
박준근 <dteam>
2020-08-14 16:07:00 +0900

    공지사항, QnA, 프로필 수정후 업로드 했습니다.

---
+++ ProjectD/app/src/main/java/com/example/projectd/NoticeSubActivity.java
@@ -0,0 +1,13 @@
+package com.example.projectd;
+
+public class NoticeSubActivity {
+
+
+
+
+
+
+
+
+
+}
--- ProjectD/app/src/main/java/com/example/projectd/ProfilActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/ProfilActivity.java
@@ -5,16 +5,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NoticeActivity extends AppCompatActivity {
-
+public class ProfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notice);
+        setContentView(R.layout.activity_profil);
         ActionBar actionBar = getSupportActionBar();  //제목줄 객체 얻어오기
-        actionBar.setTitle("공지사항");  //액션바 제목설정
+        actionBar.setTitle("프로필");  //액션바 제목설정
         actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
+
+
+
 
     }
 }
