57f1411a6fefc6940a1938a1890f7c40e0c4ef4e
박서은 <qazplm5322@hanmail.net>
2020-08-15 21:18:14 +0900

    login, sign_up_form, location xml 수정 CategoryView 액티비티 추가

--- ProjectD/app/src/main/java/com/example/projectd/LoginActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/LoginActivity.java
@@ -3,12 +3,22 @@ package com.example.projectd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView id_pw_search, signUp;
+    Button loginSubmitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        id_pw_search = findViewById(R.id.loginSubmitBtn);
+        signUp = findViewById(R.id.signUp);
+        loginSubmitBtn = findViewById(R.id.loginSubmitBtn);
+
+
+    } //onCreate()
+} //class
