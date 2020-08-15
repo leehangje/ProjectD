7fbf221b0405b2cb45e1f1efebdde20624aab30c
강두리 <rkdenfl1@gmail.com>
2020-08-15 20:59:09 +0900

    아이디/비밀번호 찾기 페이지 수정

--- ProjectD/app/src/main/java/com/example/projectd/SearchIDActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/SearchIDActivity.java
@@ -2,7 +2,11 @@ package com.example.projectd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SearchIDActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class SearchIDActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_i_d);
+
+
+
+        //뒤로가기
+        Button btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SearchIDActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
