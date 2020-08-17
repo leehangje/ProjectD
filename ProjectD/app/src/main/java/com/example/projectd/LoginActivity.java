90d85b72979161bb359098c3d4d1e5c2429fbb48
박서은 <qazplm5322@hanmail.net>
2020-08-17 10:10:39 +0900

    LoginActivity 수정

--- ProjectD/app/src/main/java/com/example/projectd/LoginActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/LoginActivity.java
@@ -2,23 +2,27 @@ package com.example.projectd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText idValueText, pwValueText;
     TextView id_pw_search, signUp;
     Button loginSubmitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
-        id_pw_search = findViewById(R.id.loginSubmitBtn);
+        id_pw_search = findViewById(R.id.id_pw_search);
         signUp = findViewById(R.id.signUp);
         loginSubmitBtn = findViewById(R.id.loginSubmitBtn);
-
+        
 
     } //onCreate()
 } //class
