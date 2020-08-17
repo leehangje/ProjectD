922f57f20c4d50c77ff5b6be2dd5b0404296a9fb
이항제 <lhj2644@naver.com>
2020-08-17 11:52:24 +0900

    Chatlist , chat 화면 완료

---
+++ ProjectD/app/src/main/java/com/example/projectd/OnChatItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.projectd;
+
+import android.view.View;
+
+public interface OnChatItemClickListener {
+    public void onItemClick(ChatAdapter.ViewHolder holder, View view, int position);
+
+}
