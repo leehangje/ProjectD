d58af9ff71859a5c67a5bd0f5f7dfbde97d1dc28
choichoong <speak2946@gmail.com>
2020-08-18 13:14:56 +0900

    오늘 수정본 연결했습니다

--- ProjectD/app/src/main/java/com/example/projectd/AlramActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/AlramActivity.java
@@ -3,18 +3,64 @@ package com.example.projectd;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AlramActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    ListView listView;
+
+    AlramSubActivity adapter;
+    ArrayList<AlramSubDTO> dtos;
+
+    protected  void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alram);
 
-        ActionBar actionBar = getSupportActionBar();  //제목줄 객체 얻어오기
-        actionBar.setTitle("알림");  //액션바 제목설정
-        actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
+        Point size = getDiviceSize();
 
+        dtos = new ArrayList<>();
+
+        listView = findViewById(R.id.listView);
+
+        adapter = new AlramSubActivity(AlramActivity.this,dtos , size);
+        adapter.addDto(new AlramSubDTO("내용","2020-8-18"));
+        adapter.addDto(new AlramSubDTO("자전거 반납","2020-9-18"));
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                AlramSubDTO dto = (AlramSubDTO) adapter.getItem(position);
+                Toast.makeText(AlramActivity.this, "선택"+position+"\n내용"+dto.getContent()+"\n날짜"+dto.getMd_rental_term()
+                        , Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+
+    private Point getDiviceSize() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealSize(size);
+        }
+        int width = size.x;
+        int height = size.y;
+
+        return  size;
     }
 }
--- ProjectD/app/src/main/java/com/example/projectd/FavListActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/FavListActivity.java
@@ -2,13 +2,61 @@ package com.example.projectd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class FavListActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    FavListSubActivity adapter;
+    ArrayList<FavListDTO> dtos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav_list);
+
+        Point size = getDiviceSize();
+
+        dtos = new ArrayList<>();
+
+        listView = findViewById(R.id.listView);
+
+        adapter = new FavListSubActivity(FavListActivity.this,dtos,size);
+        adapter.addDto(new FavListDTO("물품명","닉네임"));
+        adapter.addDto(new FavListDTO("자전거","산악인"));
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                FavListDTO dto = (FavListDTO) adapter.getItem(position);
+                Toast.makeText(FavListActivity.this, "선택"+position+"\n물품명"+dto.getMd_name()+"\n닉네임"+dto.getMember_nickname()
+                        , Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    private Point getDiviceSize() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealSize(size);
+        }
+        int width = size.x;
+        int height = size.y;
+
+        return  size;
     }
 }
