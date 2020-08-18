70025cec0aa083a0836a4b58bfb69cfc0d562332
박서은 <qazplm5322@hanmail.net>
2020-08-18 16:29:43 +0900

    LoginActivity 인텐트 수정 / 액션바 설정 시험삼아 해봄

--- ProjectD/app/src/main/java/com/example/projectd/LocationActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/LocationActivity.java
@@ -1,6 +1,7 @@
 package com.example.projectd;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -17,6 +18,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,14 +49,20 @@ public class LocationActivity extends AppCompatActivity {
     Button setupBtn, submitBtn;
     EditText searchValueText;
 
-    MarkerOptions myMarker;
+    String myAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
-        checkDangerousPermissions();
+        checkDangerousPermissions();    //위험 권한 주기
+
+        // 액션 바 설정
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.actionbar_back);
+        actionBar.setTitle("위치 찾기");
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         searchValueText = findViewById(R.id.searchValueText);
         setupBtn = findViewById(R.id.setupBtn);
@@ -113,9 +121,10 @@ public class LocationActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                myAddress = searchValueText.getText().toString();
                 Intent intent = new Intent(LocationActivity.this, SignUpFormActivity.class);
+                intent.putExtra("myAddress", myAddress);
                 startActivity(intent);
-
             }
         }); //submitBtn.setOnClickListener()
 
@@ -288,6 +297,20 @@ public class LocationActivity extends AppCompatActivity {
         return lastLocation;
     } //currentLocation()
 
+    // 액션 바 아이콘 클릭했을 때
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+
+        switch (curId) {
+            case android.R.id.home :
+                // → android.R.id.home : 메뉴바 상단의 홈 아이디
+                Toast.makeText(this, "뒤로 가기!", Toast.LENGTH_SHORT).show();
+                this.finish();      // 앱 종료
+                break;
+        }
+        return true;
+    } //onOptionsItemSelected()
 
     //----------------------------------------------------------------
     // 위험 권한
