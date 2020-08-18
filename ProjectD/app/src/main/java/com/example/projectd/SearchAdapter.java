ff64f95366e163a7b8387463ac0fce3b2352ab59
강두리 <rkdenfl1@gmail.com>
2020-08-18 11:20:30 +0900

    검색 부분 수정&추가

---
+++ ProjectD/app/src/main/java/com/example/projectd/SearchAdapter.java
@@ -0,0 +1,102 @@
+package com.example.projectd;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder>
+        implements OnSearchItemClickListener {
+    ArrayList<Search> items = new ArrayList<Search>();
+    OnSearchItemClickListener listener;
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.fragment_search, parent, false);
+
+        return new ViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Search item = items.get(position);
+        holder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void setOnItemClickListener(OnSearchItemClickListener listener){
+        this.listener = listener;
+    }
+
+
+
+    @Override
+    public void onItemClick(ViewHolder holder, View view, int position) {
+        if(listener != null){
+            listener.onItemClick(holder, view, position);
+        }
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        TextView textView2;
+
+        public ViewHolder(View itemView, final OnSearchItemClickListener listener){
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.tv_title);
+            textView2 = itemView.findViewById(R.id.tv_price);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+
+                    if(listener != null){
+                        listener.onItemClick(ViewHolder.this, view, position);
+                    }
+                }
+            });
+        }
+
+        public void setItem(Search item){
+            textView.setText(item.getTitle());
+            textView2.setText(item.getPrice());
+        }
+
+
+
+    }
+
+
+    public void addItem(Search item){
+        items.add(item);
+    }
+
+    public void setItems(ArrayList<Search> items){
+        this.items = items;
+    }
+
+    public Search getItem(int position){
+        return items.get(position);
+    }
+
+    public void setItem(int position, Search item){
+        items.set(position, item);
+    }
+
+}
