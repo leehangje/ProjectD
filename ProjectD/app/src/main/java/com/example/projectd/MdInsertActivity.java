05237cec86792b815e8b28f478d8eeded6785f14
이항제 <lhj2644@naver.com>
2020-08-19 15:06:09 +0900

    mdInsertActivity 수정

--- ProjectD/app/src/main/java/com/example/projectd/MdInsertActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/MdInsertActivity.java
@@ -31,7 +31,7 @@ public class MdInsertActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_md_insert);
 
         Spinner spinner = (Spinner)findViewById(R.id.spinner);
 
