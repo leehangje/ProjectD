980d8ee4c443759f9739324c8c8fe35dc2018131
이재은 <jaeeun1012@gmail.com>
2020-08-19 12:57:08 +0900

    상세페이지 탭 - 다른상품보기 인텐트

--- ProjectD/app/src/main/java/com/example/projectd/TabFragment2.java
+++ ProjectD/app/src/main/java/com/example/projectd/TabFragment2.java
@@ -1,61 +1,60 @@
 package com.example.projectd;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class TabFragment2 extends Fragment {
+    MdDetailActivity activity;
 
     RecyclerView recyclerView;
-    MainMdAdapter adapter;
+    DarunMdAdapter adapter;
+    ArrayList<Darun> items;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public TabFragment2() {
-
-    }
-
-
-    public static TabFragment2 newInstance(String param1, String param2) {
-        TabFragment2 fragment = new TabFragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_tab2,container,false);
 
-        MdDetailActivity activity = (MdDetailActivity) getActivity();
+        activity = (MdDetailActivity) getActivity();
+        items = new ArrayList<>();
 
         recyclerView = rootView.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(activity.getApplicationContext(), LinearLayoutManager.VERTICAL , false);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(activity.getApplicationContext(),LinearLayoutManager.VERTICAL,false));
-        adapter = new MainMdAdapter();
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new DarunMdAdapter(activity.getApplicationContext(), items);
+
+        adapter.addItem(new Darun("킥보드", "2000원"));
+        adapter.addItem(new Darun("자건거", "5000원"));
 
         recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnDarunMdItemClickListener() {
+            public static final int main = 1001;
+
+            @Override
+            public void onItemClick(DarunMdAdapter.ViewHolder holder, View view, int position) {
+                Darun item = adapter.getItem(position);
+
+                Toast.makeText(activity.getApplicationContext(), "선택됨" + item.getTitle(),
+                        Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(activity.getApplicationContext(), MdDetailActivity.class);
+                startActivityForResult(intent, main);
+            }
+        });
 
         return  rootView;
 
