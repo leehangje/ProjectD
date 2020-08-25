a87973f95fd586a29c6aea7ddcf23d1d88b82fc3
강두리 <rkdenfl1@gmail.com>
2020-08-25 19:16:33 +0900

    검색결과 부분 숨겨짐

--- ProjectD/app/src/main/java/com/example/projectd/SearchActivity.java
+++ ProjectD/app/src/main/java/com/example/projectd/SearchActivity.java
@@ -2,27 +2,45 @@ package com.example.projectd;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.Toast;
 import android.view.MenuItem;
 
+import java.util.zip.Inflater;
+
 public class SearchActivity extends AppCompatActivity {
+    EditText editText;
+    SearchView searchView;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        // 액션 바 설정
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setIcon(R.drawable.actionbar_back);
-        actionBar.setTitle("검색");
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        editText = findViewById(R.id.searchText);
+        searchView = findViewById(R.id.icon_search);
+        linearLayout = findViewById(R.id.linearlayout);
+
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                linearLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
 
 
     }//onCreate
@@ -40,4 +58,9 @@ public class SearchActivity extends AppCompatActivity {
         }
         return true;
     } //onOptionsItemSelected()
+
+
+
+
+
 }
